f7ace25d253f703666c6bd78cb6a699b47336f3a
MostafaMahmoudAshour <mostafamahmoudashour9@gmail.com>
2025-07-02 12:30:48 +0300

    Prediction Pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -23,7 +23,7 @@ from src.utils import evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('aftifacts', 'model.pkl')
+    trained_model_file_path = os.path.join('artifacts', 'model.pkl')
 
 
 class ModelTraniner:
